4c9a4c524ad95d0b9001b6b3005caf668f61462a
pnd-suburi <djreopon@gmail.com>
2017-11-02 12:52:29 +0000

    Revert "パスの文字列表示"
    This reverts commit cd2c1747ba7255380f874d78002e36cc1d8f4b0e.

--- routes/photos.js
+++ routes/photos.js
@@ -2,13 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.param('id', (req, res, next, id) => {
-  res.send(id);
-  next();
-});
-
-router.get('/:id', (req, res, next) => {
-  res.end();
+router.get('/', (req, res, next) => {
+  res.send('Some photos');
 });
 
 module.exports = router;
